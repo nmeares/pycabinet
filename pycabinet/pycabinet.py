09aa3ebb9d18a56fcbedc80ae88e570a4c01af96
Nick <nickmeares@gmail.com>
2020-09-09 22:26:30 +0100

    Speed improvements and simplification of code

--- pycabinet/pycabinet.py
+++ pycabinet/pycabinet.py
@@ -1,46 +1,72 @@
 import os, fnmatch, copy
 
-#TODO: move inner cabinet class out/ file and folder functions inside. _cabinet becomes main class.
-
 # Cabinet object class
 class cabinet:
+    """# Cabinet object
+    """  
+
     def __init__(self, path: str, item='cabinet', full_path=False):
         self.path = path
-        self.item = item
+        self.instance = item
         self.full_path = full_path
-        self.all = self._list()
+        self.index = None
+        self.inherited = None
         
-    def __str__(self): return str(self.all)
+    def __str__(self): return str(self._list)
 
-    def __iter__(self): return iter(self.all)
+    def __iter__(self): return iter(self._list)
 
     def __getitem__(self, index):
-        if type(index) == int: 
-            try: return self.all[index]
-            except IndexError: return None
-        else:
-            new = copy.deepcopy(self)
-            new.all = [f for f in self.all if fnmatch.fnmatch(f, "*" + index + "*")==True]
-            return new
-
+        new = copy.deepcopy(self)
+        new.index = index
+        new.inherited = new._indexed(new._list, new.index)
+        return new
+    
+    @property
     def _list(self):
-        # Init file list
-        if self.item=='file':
-            file_list = [f for f in os.listdir(self.path) if os.path.isfile(os.path.join(self.path,f))]
-        elif self.item=='folder':
-            file_list = [f for f in os.listdir(self.path) if os.path.isdir(os.path.join(self.path,f))]
-        else:
+        # Use inherited cabinet list or create new one
+        if self.inherited is None:
+            # Init file list
             file_list = os.listdir(self.path)
+            
+            if self.instance=='file':
+                file_list = self._listis(file_list, os.path.isfile)
+            elif self.instance=='folder':
+                file_list = self._listis(file_list, os.path.isdir)
+            else: pass # complete directory list
+            
+        else: file_list = self.inherited
 
-        # Add full path
+        # Append full path
         if self.full_path is True:
-            file_list = [os.path.join(self.path, filename) for filename in file_list]
+            file_list = self._join_path(file_list)
         else: pass
+
         return file_list
+
+    def _listis(self, lst, func):
+        check_path = lambda path: path if func(path) == True
+        return map(check_path, lst)
+
+    def _join_path(self, lst):
+        add_path = lambda filename: os.path.join(self.path, filename)
+        return list(map(add_path, lst))
+
+    def _filter(self, lst, search_string):
+        return fnmatch.filter(lst, "*" + search_string + "*")
+
+    def _indexed(self, lst, idx):
+        if isinstance(idx, (int, slice)): 
+            try: return lst[idx]
+            except IndexError: return None
+        elif isinstance(idx, str):
+            try: return self._filter(lst, idx)
+            except IndexError: return None
+
 
     @property
     def latest(self):
-        lst = [os.path.join(self.path, filename) for filename in self.all]
+        lst = self._append_path(self._list)
         return os.path.basename(max(lst, key=os.path.getctime, default="None"))
 
     def files(self, full_path=False):
@@ -48,3 +74,6 @@ class cabinet:
 
     def folders(self, full_path=False):
         return cabinet(self.path, 'folder', full_path)
+
+    def apply(self, func):
+        pass
